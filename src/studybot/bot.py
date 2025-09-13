f2e625dc194b92637a865dd72d287370d822f302
Anton Golubchikov <anton1programmist@gmail.com>
2025-09-13 20:00:20 +0700

    simple run and configure bot

--- src/studybot/bot.py
+++ src/studybot/bot.py
@@ -1,5 +1,14 @@
 """Create bot."""
 
+from telegram.ext import (
+    ApplicationBuilder,
+)
+
+from .config import Config
+
 
 def run() -> None:
     """Run study bot."""
+    config = Config()
+    app = ApplicationBuilder().token(config.TELEGRAM_TOKEN).build()
+    app.run_polling()
--- src/studybot/config.py
+++ src/studybot/config.py
@@ -2,12 +2,12 @@
 
 from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_PATH = Path(__file__).parent.parent
 
 
-class Config(BaseModel):
+class Config(BaseSettings):
     """
     cofig class.
 
@@ -16,3 +16,12 @@ class Config(BaseModel):
     BaseModel : BaseModel
         BaseModel class. Pydantic class.
     """
+
+    BASE_PATH: Path = BASE_PATH
+
+    TELEGRAM_TOKEN: str = ""
+
+    model_config = SettingsConfigDict(env_file=BASE_PATH.parent / ".env")
+
+
+config = Config()
