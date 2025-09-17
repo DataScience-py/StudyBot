7fa45d601dbd2c5c65e7099955655d98e2a23237
Anton Golubchikov <anton1programmist@gmail.com>
2025-09-17 20:00:22 +0700

    refact code

--- src/studybot/meassage/check_task.py
+++ src/studybot/meassage/check_task.py
@@ -22,25 +22,23 @@ async def check_task_handler(
 
     if config.TASK_ANSWER in user_data:
         last_answer = user_data[config.TASK_ANSWER]
-        if last_answer == user_answer:
-            await update.message.reply_text("Верно!")
-            del user_data[config.TASK_ANSWER]
-            del user_data[config.TASK_TEXT]
-        else:
-            await update.message.reply_text("Не верно!")
     else:
         subject = user_data.get(config.SUBJECTS)
         number = user_data.get(config.NUMBER)
         task = user_data.get(config.TASK)
         if subject is None or number is None or task is None:
-            await update.message.reply_text("Выберите задание заново!")
+            await update.message.reply_text(
+                "Выберите сначала задание. Используйте /tasks!",
+            )
             return
         task = await db.get_task(subject=subject, number=number, task_id=task)
         user_data[config.TASK_TEXT] = task[config.TASK_TEXT]
-        user_data[config.TASK_ANSWER] = answer = task[config.TASK_ANSWER]
-        if user_answer == answer:
-            await update.message.reply_text("Верно!")
-        else:
-            await update.message.reply_text("Не верно!")
+        user_data[config.TASK_ANSWER] = last_answer = task[config.TASK_ANSWER]
+    if user_answer == last_answer:
+        del user_data[config.TASK_ANSWER]
+        del user_data[config.TASK_TEXT]
+        await update.message.reply_text("Верно!")
+    else:
+        await update.message.reply_text("Не верно!")
 
     await db.update_user_data(user_id, user_data)
